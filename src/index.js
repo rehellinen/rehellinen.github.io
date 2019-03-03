29e46b66f8972fad79c3872d374c4e0e07e3c499
rehellinen <912377791@qq.com>
2019-03-03 09:46:40 +0800

    dev environment

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/**
+ *  index.js
+ *  Create By rehellinen
+ *  Create On 2018/11/28 10:24
+ */
+import Vue from 'vue'
+import App from './App'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
