8d1945442699c4b3dd99e77563dccef1112153ff
rehellinen <912377791@qq.com>
2019-03-13 16:06:07 +0800

    client --- detail page

---
+++ client/base/side-bar/side-bar.vue
@@ -0,0 +1,65 @@
+<template lang="pug">
+  div.side-bar-container
+    div.recommend
+      p.title Most Popular Articles
+      div.article(
+        v-for="article in popularArticles"
+        :key="article.id"
+      )
+        img(:src="article.main_img_url")
+        div.text-container
+          p.title {{article.title}}
+          p.subtitle {{article.subtitle}}
+      .line
+</template>
+
+<script>
+  import {ArticleModel} from "../../model/ArticleModel"
+
+  const article = new ArticleModel()
+
+  export default {
+    created () {
+      this._getData()
+    },
+    data () {
+      return {
+        popularArticles: []
+      }
+    },
+    methods: {
+      async _getData () {
+        this.popularArticles = await article.getPopularArticles()
+      }
+    }
+}
+</script>
+
+<style scoped lang="sass" rel="stylesheet/sass">
+  @import "~sass/base"
+
+  .side-bar-container
+    padding-left: 10%
+    .recommend
+      display: flex
+      flex-direction: column
+      .title
+        font-size: $big-font-size
+        font-weight: bold
+        color: $normal-font-color
+  .article
+    display: flex
+    margin-top: 10px
+    img
+      width: 70px
+      height: 70px
+    .text-container
+      margin-left: 10px
+      .title
+        font-size: $normal-font-size
+        font-weight: bold
+      .subtitle
+        color: $minor-font-color
+        font-size: $small-font-size
+  .de
+</style>
---
+++ client/components/index/index.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  div
+    article-main(v-for="article in articles" :article="article" :key="article.id")
+</template>
+
+<script>
+  import ArticleMain from '../../base/article-main/article-main'
+  import {ArticleModel} from "../../model/ArticleModel"
+
+  const article = new ArticleModel()
+
+  export default {
+    data () {
+      return {
+        articles: []
+      }
+    },
+    async created () {
+      this.articles = await article.getArticles()
+    },
+    components: {
+      ArticleMain
+    }
+  }
+</script>
+
+<style scoped lang="sass" rel="stylesheet/sass">
+
+</style>
