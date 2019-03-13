30a2b39e87b32d9b6194581426ab0c984f05f5e8
rehellinen <912377791@qq.com>
2019-03-13 16:10:22 +0800

    client --- index page optimize

--- client/components/index/index.vue
+++ client/components/index/index.vue
@@ -1,10 +1,16 @@
 <template lang="pug">
-  div
-    article-main(v-for="article in articles" :article="article" :key="article.id")
+  div.article-container
+    div.one-article(v-for="article in articles" :article="article" :key="article.id")
+      router-link.article(:to="{path: `/detail/${article.id}`}" tag="div")
+        div.zoom
+          div(:style="imgStyle(article)")
+        div.text-container
+          p.title {{article.title}}
+          p.subtitle {{article.subtitle}}
+      div.line
 </template>
 
 <script>
-  import ArticleMain from '../../base/article-main/article-main'
   import {ArticleModel} from "../../model/ArticleModel"
 
   const article = new ArticleModel()
@@ -18,12 +24,42 @@
     async created () {
       this.articles = await article.getArticles()
     },
-    components: {
-      ArticleMain
+    methods: {
+      imgStyle (article) {
+        return {
+          'background-image': `url(${article.main_img_url})`
+        }
+      }
     }
   }
 </script>
 
 <style scoped lang="sass" rel="stylesheet/sass">
+  @import "~sass/base"
 
+  .one-article
+    display: flex
+    flex-direction: column
+    a
+      text-decoration: none
+    .article
+      display: flex
+      .zoom
+        width: 350px
+        div
+          width: 100%
+          height: 0
+          padding-bottom: 55%
+          overflow: hidden
+          background: no-repeat center center
+          background-size: cover
+      .text-container
+        margin-left: 6%
+        .title
+          font-size: $bigger-font-size
+          font-weight: bold
+        .subtitle
+          font-size: $normal-font-color
+          color: $minor-font-color
+          margin-top: 5px
 </style>
